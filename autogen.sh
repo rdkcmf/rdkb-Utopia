863ce231164e3a88e01d1680e38eb2c702e4722a
Steve Maynard <steve.maynard@twcable.com>
2014-08-11 10:09:00 -0600

    moved all Makefiles to *.orig

---
+++ autogen.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+#
+test -n "$srcdir" || srcdir=`dirname $0`
+test -n "$srcdir" || srcdir=.
+olddir=`pwd`
+cd $srcdir
+AUTORECONF=`which autoreconf`
+
+if test -z $AUTORECONF; then
+	echo '*** no autoreconf found. please install it ***'
+	exit 1
+fi
+autoreconf --force --install --verbose || exit $?
+cd $olddir || exit $?
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
