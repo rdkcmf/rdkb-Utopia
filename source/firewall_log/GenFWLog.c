1d22eaa64b5f7be00caab02ee1a07496ef0ddf79
Steve Maynard <steve.maynard@twcable.com>
2014-11-17 11:56:12 -0700

    added dhcpproxy and firewall_log; started firewall and igd

--- source/firewall_log/GenFWLog.c
+++ source/firewall_log/GenFWLog.c
@@ -112,7 +112,7 @@ int fLock(){
     fl.l_len = 0;  
     fl.l_pid = -1;  
 
-    if((fd=open(LOCK_FILE_NAME,O_CREAT| O_RDWR))>0){
+    if((fd=open(LOCK_FILE_NAME,O_CREAT| O_RDWR, S_IRUSR | S_IWUSR))>0){
         do{
             if( -1  != fcntl(fd,F_SETLK,&fl)){
                 printf("GET W-LOCK SUCCESS\n");
