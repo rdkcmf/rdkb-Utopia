c8e4d6ca6f7dadd1a4667e6431404f56068b1387
Michael L. Walker <michael.l.walker@intel.com>
2016-05-19 19:51:40 -0700

    ARRISXB6-1435 : xb6 bridge handler needs to do more lan setup
    Reason for change: xb6 bridge handler is called when the bridge-start event
            occurs, instead of the lan-start event being raised, so the handler
            script needs to make sure the bridges are created and connected, and
            configure an interface named lan0 that lighttpd can listen on
    Test Procedure: build and test on cougar park platform
    Risks: None
    
    Change-Id: I3249397ea788d377c25416c7594acf705b4aa633
    Signed-off-by: Michael Walker <michael.l.walker@intel.com>

--- source/scripts/init/service.d/service_bridge_puma7.sh
+++ source/scripts/init/service.d/service_bridge_puma7.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 #Script to put the private LAN into pseudo bridge mode
-#This script assumes that LAN bridge is already set up with all LAN ports connected
+
+#source /etc/utopia/service.d/interface_functions.sh
+source /etc/utopia/service.d/hostname_functions.sh
+source /etc/utopia/service.d/ulog_functions.sh
+#source /etc/utopia/service.d/service_lan/wlan.sh
+source /etc/utopia/service.d/event_handler_functions.sh
+#source /etc/utopia/service.d/brcm_ethernet_helper.sh
 
 SERVICE_NAME="bridge"
 
 #Which multinet instance is the private LAN?
 INSTANCE=1
 
-#Name of the multinet bridge we are putting into this mode
-LAN_BRIDGE="`sysevent get multinet_${INSTANCE}-name`"
-
-#IP address of the LAN interface (which will listen for the web UI connections)
-LAN_IP="`sysevent get current_lan_ipaddr`"
+#TODO: Should we get these from CCSP instead of hardcoding them?
+LAN_IP="10.0.0.1"
+LAN_NETMASK="255.255.255.0"
 
 #Separate routing table used to ensure that responses from the web UI go directly to the LAN interface, not out erouter0 
 BRIDGE_MODE_TABLE=69
@@ -19,9 +23,61 @@ BRIDGE_MODE_TABLE=69
 #Mode passed in by commandline, can be "enable" or "disable"
 SCRIPT_MODE="$1"
 
+wait_till_steady_state ()
+{
+	LSERVICE=$1
+	TRIES=1
+	while [ "30" -ge "$TRIES" ] ; do
+		LSTATUS=`sysevent get ${LSERVICE}-status`
+		if [ "starting" = "$LSTATUS" ] || [ "stopping" = "$LSTATUS" ] || [ "partial" = "$LSTATUS" ] ; then
+			sleep 1
+			TRIES=`expr $TRIES + 1`
+		else
+			return
+		fi
+	done
+	echo "$0: Timed out waiting for $LSERVICE to be in a steady state"
+}
+
+wait_till_started ()
+{
+	LSERVICE=$1
+	TRIES=1
+	while [ "30" -ge "$TRIES" ] ; do
+		LSTATUS=`sysevent get ${LSERVICE}-status`
+		if [ "started" = "$LSTATUS" -o "ready" = "$LSTATUS" ] ; then
+			return
+		else
+			sleep 1
+			TRIES=`expr $TRIES + 1`
+		fi
+	done
+	echo "$0: Timed out waiting for $LSERVICE to be started or ready"
+}
+
+
+wait_till_stopped()
+{
+	LSERVICE=$1
+	TRIES=1
+	while [ "30" -ge "$TRIES" ] ; do
+		LSTATUS=`sysevent get ${LSERVICE}-status`
+		if [ "stopped" = "$LSTATUS" ] ; then
+			return
+		else
+			sleep 1
+			TRIES=`expr $TRIES + 1`
+		fi
+	done
+	echo "$0: Timed out waiting for $LSERVICE to be stopped"
+}
+
 flush_connection_info(){
 	#Flush packet processor sessions on NPCPU
 	ncpu_exec -e "(echo flush_all_sessions > /proc/net/ti_pp)"
+
+	#Flush CPE table
+	ncpu_exec -e "(echo \"LearnFrom=CPE_DYNAMIC\" > /proc/net/dbrctl/delalt)"
 
 	#Flush connection tracking entries
 	#NOTE: Disabled for now, there is an Intel bug where this command freezes.  Uncomment once that fix is merged to Comcast
@@ -29,27 +85,15 @@ flush_connection_info(){
 }
 
 #Add or remove rules to block local traffic from reaching DOCSIS bridge
-#This is to prevent packets coming from APPCPU network stack from corrupting the CPE mac address table
 filter_local_traffic(){
 	FILTER_MODE=$1
 
 	if [ "$FILTER_MODE" = "enable" ] ; then
-	        #Get lbr0 mac address to block / unblock
-	        LBR0_MAC="`cat /sys/class/net/lbr0/address`"
-	        LLBR0_MAC="`cat /sys/class/net/llbr0/address`"
-
 		#Block locally-generated traffic from LAN bridge from going to DOCSIS bridge and corrupting CPE table
 		ebtables -A OUTPUT -o lbr0 -j DROP
-		ebtables -N PSEUDO_BRIDGE_FORWARD_FILTER
-		ebtables -I FORWARD -j PSEUDO_BRIDGE_FORWARD_FILTER
-		ebtables -A PSEUDO_BRIDGE_FORWARD_FILTER -s $LBR0_MAC -j DROP
-		ebtables -A PSEUDO_BRIDGE_FORWARD_FILTER -s $LLBR0_MAC -j DROP
-		ebtables -A PSEUDO_BRIDGE_FORWARD_FILTER -j RETURN
 	else
 		#Unblock locally-generated traffic from LAN bridge from going to DOCSIS bridge and corrupting CPE table
 		ebtables -D OUTPUT -o lbr0 -j DROP 2> /dev/null
-		ebtables -D FORWARD -j PSEUDO_BRIDGE_FORWARD_FILTER 2> /dev/null
-		ebtables -X PSEUDO_BRIDGE_FORWARD_FILTER 2> /dev/null
 	fi
 }
 
@@ -63,75 +107,209 @@ unblock_bridge(){
 	ebtables -D FORWARD -i llbr0 -j DROP
 }
 
-#Delete configuration that might apply to an old, stale IP or mac address, in case LAN bridge gets reconfigured
-delete_stale_entries(){
-	ebtables -t nat -D PREROUTING -j PSEUDO_BRIDGE_REDIRECT 2> /dev/null
-	ebtables -t nat -X PSEUDO_BRIDGE_REDIRECT 2> /dev/null
-	iptables -D INPUT -i $LAN_BRIDGE -p udp --dport 67 -j DROP 2> /dev/null
-	ip rule del lookup $BRIDGE_MODE_TABLE 2> /dev/null
-	ip route flush table $BRIDGE_MODE_TABLE 2> /dev/null
-	filter_local_traffic disable 2> /dev/null
+restart_webgui()
+{
+	killall lighttpd
+	/etc/webgui.sh
+}
+
+do_start_multi()
+{
+	PRI_L2=`sysevent get primary_lan_l2net`
+	#This is to address a BUG in vlan_util_xb6.sh that it doesn't set state to starting
+	sysevent set multinet-start $PRI_L2
+	wait_till_started multinet_${PRI_L2}
+}
+
+do_stop_multi()
+{
+	PRI_L2=`sysevent get primary_lan_l2net`
+	sysevent set multinet-stop $PRI_L2
+	wait_till_stopped multinet_${PRI_L2}
+}
+
+
+cmdiag_ebtables_rules()
+{
+	if [ "$1" = "enable" ] ; then
+		CMDIAG_MAC="`cat /sys/class/net/lan0/address`"
+
+		#Don't allow lan0 to send traffic to DOCSIS bridge
+		ebtables -N BRIDGE_FORWARD_FILTER
+		ebtables -F BRIDGE_FORWARD_FILTER 2> /dev/null
+		ebtables -I FORWARD -j BRIDGE_FORWARD_FILTER
+		ebtables -A BRIDGE_FORWARD_FILTER -s $CMDIAG_MAC -o lbr0 -j DROP
+		ebtables -A BRIDGE_FORWARD_FILTER -j RETURN
+
+		#Redirect traffic destined to lan0 IP to lan0 MAC address
+		ebtables -t nat -N BRIDGE_REDIRECT
+		ebtables -t nat -F BRIDGE_REDIRECT 2> /dev/null
+		ebtables -t nat -I PREROUTING -j BRIDGE_REDIRECT
+		ebtables -t nat -A BRIDGE_REDIRECT -p ipv4 --ip-dst $LAN_IP -j dnat --to-destination $CMDIAG_MAC
+		ebtables -t nat -A BRIDGE_REDIRECT -j RETURN
+	else
+		ebtables -D FORWARD -j BRIDGE_FORWARD_FILTER
+		ebtables -X BRIDGE_FORWARD_FILTER
+		ebtables -t nat -D PREROUTING -j BRIDGE_REDIRECT
+		ebtables -t nat -X BRIDGE_REDIRECT
+	fi
+}
+
+#Create a virtual lan0 management interface and connect it to the bride
+#Also prevent this interface from sending any packets to the DOCSIS bridge
+cmdiag_if()
+{
+	if [ "$1" = "enable" ] ; then
+		ip link add $CMDIAG_IF type veth peer name l${CMDIAG_IF} 
+		echo 1 > /proc/sys/net/ipv6/conf/llan0/disable_ipv6
+		ifconfig $CMDIAG_IF hw ether $CMDIAG_MAC
+		cmdiag_ebtables_rules enable
+		ifconfig l${CMDIAG_IF} promisc up
+		ifconfig $CMDIAG_IF $LAN_IP netmask $LAN_NETMASK up
+		vlan_util add_interface $LAN_BRIDGE l${CMDIAG_IF}
+	else
+		vlan_util del_interface $LAN_BRIDGE l${CMDIAG_IF}
+		ifconfig $CMDIAG_IF down
+		ifconfig l${CMDIAG_IF} down
+		ip link del $CMDIAG_IF
+		cmdiag_ebtables_rules disable
+	fi
 }
 
 #Enable pseudo bridge mode.  If already enabled, just refresh parameters (in case bridges were torn down and rebuilt)
-enable_bridge_mode(){
+service_start(){
+	wait_till_steady_state ${SERVICE_NAME}
+	STATUS=`sysevent get ${SERVICE_NAME}-status`
+	if [ "started" != "$STATUS" ] ; then
+		do_start_multi
 
-	block_bridge
+		sysevent set ${SERVICE_NAME}-errinfo
+		sysevent set ${SERVICE_NAME}-status starting
 
-	delete_stale_entries
+		block_bridge
 
-	#Block DHCP requests on $LAN_BRIDGE
-	iptables -I INPUT -i $LAN_BRIDGE -p udp --dport 67 -j DROP
+		#Block DHCP requests on $LAN_BRIDGE
+		iptables -I INPUT -i $CMDIAG_IF -p udp --dport 67 -j DROP
 
-	#Receive packets destined to lan IP
-	ebtables -t nat -N PSEUDO_BRIDGE_REDIRECT
-	ebtables -t nat -I PREROUTING -j PSEUDO_BRIDGE_REDIRECT
-	ebtables -t nat -A PSEUDO_BRIDGE_REDIRECT --logical-in $LAN_BRIDGE -p ipv4 --ip-dst $LAN_IP -j redirect --redirect-target ACCEPT
-	ebtables -t nat -A PSEUDO_BRIDGE_REDIRECT -j RETURN
+		#Connect management interface
+		cmdiag_if enable
 
-	#Send responses from $LAN_BRIDGE IP to a separate bridge mode route table
-	ip rule add from $LAN_IP lookup $BRIDGE_MODE_TABLE
-	ip route add table $BRIDGE_MODE_TABLE default dev $LAN_BRIDGE
+		#Send responses from $LAN_BRIDGE IP to a separate bridge mode route table
+		ip rule add from $LAN_IP lookup $BRIDGE_MODE_TABLE
+		ip route add table $BRIDGE_MODE_TABLE default dev $CMDIAG_IF
 
-	#Add lbr0 to $LAN_BRIDGE
-	vlan_util add_interface $LAN_BRIDGE lbr0
+		#Add lbr0 to $LAN_BRIDGE
+		vlan_util add_interface $LAN_BRIDGE lbr0
 
-	#Flush connection tracking and packet processor sessions to avoid stale information
-	flush_connection_info
+		#Flush connection tracking and packet processor sessions to avoid stale information
+		flush_connection_info
+	
+		#Block traffic coming from the lbr0 connector interfaces at the MUX
+		filter_local_traffic enable
 
-	#Block traffic coming from the lbr0 connector interfaces at the MUX
-	filter_local_traffic enable
+		unblock_bridge
 
-	unblock_bridge	
+		prepare_hostname
+
+		restart_webgui
+
+		gw_lan_refresh
+
+		sysevent set ${SERVICE_NAME}-errinfo
+		sysevent set ${SERVICE_NAME}-status started
+		
+	fi
 }
 
-disable_bridge_mode(){
+service_stop(){
+	wait_till_steady_state ${SERVICE_NAME}
+	STATUS=`sysevent get ${SERVICE_NAME}-status`
+	if [ "stopped" != "$STATUS" ] ; then
 
-	block_bridge
+		sysevent set ${SERVICE_NAME}-errinfo
+		sysevent set ${SERVICE_NAME}-status stopping
 
-	delete_stale_entries
+		block_bridge
 
-	#Remove local bridge connection from 
-	vlan_util del_interface $LAN_BRIDGE lbr0
+		#Remove local bridge connection from 
+		vlan_util del_interface $LAN_BRIDGE lbr0
 
-	#Flush connection tracking and packet processor sessions to avoid stale information
-	flush_connection_info
+		#Disconnect management interface
+		cmdiag_if disable
+		filter_local_traffic disable
+	
+		#Flush connection tracking and packet processor sessions to avoid stale information
+		flush_connection_info
 
-	unblock_bridge
+		unblock_bridge
+		do_stop_multi
+
+		iptables -D INPUT -i $CMDIAG_IF -p udp --dport 67 -j DROP
+
+		restart_webgui
+
+		gw_lan_refresh
+
+		sysevent set ${SERVICE_NAME}-errinfo
+		sysevent set ${SERVICE_NAME}-status stopped
+
+	fi
 }
+
+#--------------------------------------------------------------
+# service_init
+#--------------------------------------------------------------
+service_init ()
+{
+   # Get all provisioning data
+   # Figure out the names and addresses of the lan interface
+   #
+   # SYSCFG_lan_ethernet_physical_ifnames is the physical ethernet interfaces that
+   # will be part of the lan
+   #
+   # SYSCFG_lan_wl_physical_ifnames is the names of each wireless interface as known
+   # to the operating system
+
+   SYSCFG_FAILED='false'
+   FOO=`utctx_cmd get bridge_mode lan_ifname lan_ethernet_physical_ifnames lan_wl_physical_ifnames wan_physical_ifname bridge_ipaddr bridge_netmask bridge_default_gateway bridge_nameserver1 bridge_nameserver2 bridge_nameserver3 bridge_domain hostname`
+   eval $FOO
+  if [ $SYSCFG_FAILED = 'true' ] ; then
+     ulog bridge status "$PID utctx failed to get some configuration data"
+     ulog bridge status "$PID BRIDGE CANNOT BE CONTROLLED"
+     exit
+  fi
+
+  if [ -z "$SYSCFG_hostname" ] ; then
+     SYSCFG_hostname="Utopia"
+  fi
+
+  LAN_IFNAMES="$SYSCFG_lan_ethernet_physical_ifnames"
+
+   # if we are using wireless interfafes then add them
+   if [ "" != "$SYSCFG_lan_wl_physical_ifnames" ] ; then
+      LAN_IFNAMES="$LAN_IFNAMES $SYSCFG_lan_wl_physical_ifnames"
+   fi
+}
+
 
 echo "service_bridge_puma7.sh called with $1 $2" > /dev/console
+service_init
+
+LAN_BRIDGE="$SYSCFG_lan_ifname"
+CMDIAG_IF=`syscfg get cmdiag_ifname`
+CMDIAG_MAC=`ncpu_exec -ep "(cat /sys/class/net/lan0/address)"`
+
 case "$1" in
    ${SERVICE_NAME}-start)
-      enable_bridge_mode
+      service_start
       ;;
    ${SERVICE_NAME}-stop)
-      disable_bridge_mode
+      service_stop
       ;;
    ${SERVICE_NAME}-restart)
       sysevent set lan-restarting 1
-      disable_bridge_mode
-      enable_bridge_mode
+      service_stop
+      service_start
       sysevent set lan-restarting 0
       ;;
    *)
