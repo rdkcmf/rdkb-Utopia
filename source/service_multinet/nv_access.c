c3df64fdc7728d20347e062a896385a2c6ee703b
sramani <Santosh_Ramani@infosys.com>
2016-06-10 21:40:12 +0530

    RDKB-6050 : To remove all psmcli call from service_multinet process.
    Reason for change: Need to remove all psmcli call from service_multinet process and then we need to replace this call by PSM_Get_Record_Value2() common dbus API.
    Test Procedure:
    1. Verify the brlan0 and brlan1 bridge status by brctl show command after bridge creation. It should be createwith proper vlan interfaces l2sd0.
    2. Ensure dnsmasq running with check all process states
    Risks: None
    Signed-off-by: Santosh_Ramani Santosh_Ramani@infosys.com
    
    Change-Id: I9ea7d43f990e62159f6e5bbed8b678936a4467cd

--- source/service_multinet/nv_access.c
+++ source/service_multinet/nv_access.c
@@ -35,13 +35,64 @@
 #include "service_multinet_nv.h"
 #include <stdio.h>
 #include <string.h>
-
+#ifdef _COSA_INTEL_XB3_ARM_
+#include "ccsp_custom.h"
+#include "ccsp_psm_helper.h"
+#include <ccsp_base_api.h>
+#include "ccsp_memory.h"
+#endif /* _COSA_INTEL_XB3_ARM_ */
 char* typeStrings[] = {
     "SW", "Gre", "Link", "Eth", "WiFi"
 };
 
 
-int nv_get_members(PL2Net net, PMember memberList, int numMembers) {
+const char* const multinet_component_id = "ccsp.multinet";
+static void* 	  bus_handle = NULL;
+
+#ifdef _COSA_INTEL_XB3_ARM_
+#define CCSP_SUBSYS 	"eRT."
+#define PSM_VALUE_GET_STRING(name, str) PSM_Get_Record_Value2(bus_handle, CCSP_SUBSYS, name, NULL, &(str))
+
+int dbusInit( void )
+{
+	int   ret  = -1;
+	char* pCfg = CCSP_MSG_BUS_CFG;
+
+	if( bus_handle == NULL ) 
+	{
+		MNET_DEBUG("dbusInit, called\n")
+
+		// Dbus connection init
+		#ifdef DBUS_INIT_SYNC_MODE
+			ret = CCSP_Message_Bus_Init_Synced(multinet_component_id, 
+								               pCfg, 
+								               &bus_handle, 
+								               Ansc_AllocateMemory_Callback, 
+								               Ansc_FreeMemory_Callback);
+		#else
+			ret = CCSP_Message_Bus_Init(multinet_component_id, 
+								        pCfg, 
+								        &bus_handle, 
+								        Ansc_AllocateMemory_Callback, 
+								        Ansc_FreeMemory_Callback);
+		#endif /* DBUS_INIT_SYNC_MODE */
+	}
+
+	if ( ret == -1 )
+	{
+		// Dbus connection error
+		fprintf(stderr, " DBUS connection error\n");
+		bus_handle = NULL;
+	}
+
+	return ret;
+}
+#endif /* _COSA_INTEL_XB3_ARM_  */
+
+int nv_get_members(PL2Net net, PMember memberList, int numMembers) 
+{
+/* Use to get psm value via dbus instead of psmcli util */
+#ifndef _COSA_INTEL_XB3_ARM_
     int i;
     char cmdBuff[512];
     char valBuff[256];
@@ -80,16 +131,85 @@ int nv_get_members(PL2Net net, PMember memberList, int numMembers) {
             ifToken = strtok(NULL, "\" \n");
         }
     }
-    
+#else
+    char  cmdBuff[512];
+    char  *ifToken, 
+		  *dash,
+		  *pStr = NULL;
+    int   actualNumMembers = 0,
+		  i,
+		  rc;
+
+	/* dbus init based on bus handle value */
+	if(bus_handle == NULL)
+	dbusInit( );
+
+	if(bus_handle == NULL)
+	{
+		MNET_DEBUG("nv_get_members, Dbus init error\n")
+		return 0;
+	}
+
+    for (i = 0; i < sizeof(typeStrings)/sizeof(*typeStrings); ++i) 
+	{
+		/* dmsb.l2net.%d.Members.%s*/
+		memset(cmdBuff,0,sizeof(cmdBuff));
+		snprintf(cmdBuff, sizeof(cmdBuff),"dmsb.l2net.%d.Members.%s",net->inst, typeStrings[i]); 
+
+		MNET_DEBUG("nv_get_members, lookup string %s index %d\n" COMMA cmdBuff COMMA i)
+		
+		rc = PSM_VALUE_GET_STRING(cmdBuff, pStr);
+
+		if(rc == CCSP_SUCCESS && pStr != NULL)
+		{
+			ifToken = strtok(pStr, "\" \n");
+	
+			while(ifToken) 
+			{ 
+				if ((dash = strchr(ifToken, '-')))
+				{
+					*dash = '\0';
+					memberList[actualNumMembers].bTagging = 1;
+				} 
+				else 
+				{
+					memberList[actualNumMembers].bTagging = 0;
+				}
+				
+				memberList[actualNumMembers].interface->map = NULL; // No mapping has been performed yet
+				strcpy(memberList[actualNumMembers].interface->name, ifToken);
+
+				MNET_DEBUG("%s, interface %s\n" COMMA __FUNCTION__ COMMA memberList[actualNumMembers].interface->name )
+
+				strcpy(memberList[actualNumMembers++].interface->type->name, typeStrings[i]);
+
+				if (dash) *dash = '-'; // replace character just in case it would confuse strtok
+				
+				ifToken = strtok(NULL, "\" \n");
+			}
+
+			Ansc_FreeMemory_Callback(pStr);
+			pStr = NULL;
+		} 
+   	}
+#endif /* !_COSA_INTEL_XB3_ARM_ */
+
     return actualNumMembers;
     
 }
 
-int nv_get_bridge(int l2netInst, PL2Net net) {
+int nv_get_bridge(int l2netInst, PL2Net net) 
+{
     char cmdBuff[512];
+	int  rc;
+	char *pStr = NULL;
+
+/* Use to get psm value via dbus instead of psmcli util */
+#ifndef _COSA_INTEL_XB3_ARM_
     char valBuff[80];
     char tmpBuf[15];
     FILE* psmcliOut;
+
     snprintf(cmdBuff, sizeof(cmdBuff), 
             "psmcli get -e X dmsb.l2net.%d.Name X dmsb.l2net.%d.Vid X dmsb.l2net.%d.Enable", 
              l2netInst, l2netInst, l2netInst);
@@ -113,6 +233,64 @@ int nv_get_bridge(int l2netInst, PL2Net net) {
     net->inst = l2netInst;
     
     pclose(psmcliOut);
-    
+#else
+
+	/* dbus init based on bus handle value */
+ 	if(bus_handle == NULL)
+	dbusInit( );
+
+	if(bus_handle == NULL)
+	{
+		MNET_DEBUG("nv_get_bridge, Dbus init error\n")
+		return 0;
+	}
+
+	/* dmsb.l2net.%d.Name */
+	memset(cmdBuff,0,sizeof(cmdBuff));
+    snprintf(cmdBuff, sizeof(cmdBuff),"dmsb.l2net.%d.Name",l2netInst); 
+
+	rc = PSM_VALUE_GET_STRING(cmdBuff, pStr);
+	if(rc == CCSP_SUCCESS && pStr != NULL)
+	{
+		 strcpy(net->name, pStr);
+		 Ansc_FreeMemory_Callback(pStr);
+		 pStr = NULL;
+	} 
+
+	/* dmsb.l2net.%d.Vid */
+	memset(cmdBuff,0,sizeof(cmdBuff));
+	snprintf(cmdBuff, sizeof(cmdBuff),"dmsb.l2net.%d.Vid",l2netInst);
+
+	rc = PSM_VALUE_GET_STRING(cmdBuff, pStr);
+	if(rc == CCSP_SUCCESS && pStr != NULL)
+	{
+		 net->vid = atoi(pStr);
+		 Ansc_FreeMemory_Callback(pStr);
+		 pStr = NULL;
+	}  
+
+	/* dmsb.l2net.%d.Enable */
+	memset(cmdBuff,0,sizeof(cmdBuff));
+	snprintf(cmdBuff, sizeof(cmdBuff),"dmsb.l2net.%d.Enable",l2netInst); 
+
+	rc = PSM_VALUE_GET_STRING(cmdBuff, pStr);
+	if(rc == CCSP_SUCCESS && pStr != NULL)
+	{
+		 if(!strcmp("FALSE", pStr)) 
+		 {
+			net->bEnabled = 0;
+		 }
+		 else 
+		 {
+			net->bEnabled = 1;
+		 }
+
+		 Ansc_FreeMemory_Callback(pStr);
+		 pStr = NULL;
+	}  
+
+	net->inst = l2netInst;
+#endif /* !_COSA_INTEL_XB3_ARM_ */
+
     return 0;
 }
