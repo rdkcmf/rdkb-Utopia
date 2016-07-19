463b3a1152eb244b22fe546895e6c7ca03ed5652
sshrivastav <Shirish_S@infosys.com>
2016-07-19 21:41:10 +0530

    RDKB-6718 : Adding support in Utopia to enable logging to nvram2
    Reason for change: Adding support for logging into nvram2
    Test Procedure:
    -> Set below parameters to enable logging.
    dmcli eRT setv Device.LogBackup.X_RDKCENTRAL-COM_Enable bool <true/false>
    dmcli eRT setv Device.LogBackup.X_RDKCENTRAL-COM_Interval uint <value>
    -> logs from /rdklogs/logs will be written to /nvram2/logs after each Interval.
    -> Whenever logs size increases more than expected, logs will be uploaded from nvram2 and deleted.
    Risks: None
    Signed-off-by: Shirish_S@infosys.com
    
    Change-Id: Ia94bc603b8d60955b1e1a0bfd7180a3b8fae2623

--- source/scripts/init/service.d/log_env_var.sh
+++ source/scripts/init/service.d/log_env_var.sh
@@ -10,9 +10,24 @@ LOG_UPLOAD_FOLDER="/nvram"
 RDK_LOGGER_PATH="/fss/gw/rdklogger"
 LOG_PATH="$LOG_FOLDER/logs/"
 
-LOG_BACK_UP_PATH="$LOG_UPLOAD_FOLDER/logbackup/"
-LOGTEMPPATH="$LOG_FOLDER/backuplogs/"
-LOG_BACK_UP_REBOOT="$LOG_UPLOAD_FOLDER/logbackupreboot/"
+backupenable=`syscfg get logbackup_enable`
+isNvram2Supported="no"
+if [ -f /etc/device.properties ]
+then
+   isNvram2Supported=`cat /etc/device.properties | grep NVRAM2_SUPPORTED | cut -f2 -d=`
+fi
+
+#if [ "$isNvram2Supported" = "yes" ] && [ "$backupenable" = "true" ]
+#then
+ LOG_SYNC_PATH="/nvram2/logs/"
+ LOG_SYNC_BACK_UP_PATH="/nvram2/logs/"
+ LOG_SYNC_BACK_UP_REBOOT_PATH="/nvram2/logs/"
+#else
+ LOG_BACK_UP_PATH="$LOG_UPLOAD_FOLDER/logbackup/"
+ LOGTEMPPATH="$LOG_FOLDER/backuplogs/"
+ LOG_BACK_UP_REBOOT="$LOG_UPLOAD_FOLDER/logbackupreboot/"
+#fi
+
 
 HAVECRASH="$LOG_FOLDER/processcrashed"
 FLAG_REBOOT="$LOG_FOLDER/waitingreboot"
@@ -50,6 +65,6 @@ LOG_FILE_FLAG="$LOG_FOLDER/filescreated"
 CONSOLEFILE="$LOG_FOLDER/logs/ArmConsolelog.txt.0"
 
 SELFHEALFILE="$LOG_FOLDER/logs/SelfHeal.txt.0"
-
+SELFHEALFILE_BOOTUP="$LOG_SYNC_PATH/SelfHealBootUp.txt.0"
 lockdir=$LOG_FOLDER/rxtx
 
