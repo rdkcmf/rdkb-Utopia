e748e5cb431cf192325d163dd6f14861f01e7525
Raghavendra Kammara <raghavendra_kammara@cable.comcast.com>
2016-07-21 00:06:14 +0000

    RDKB-6574 : Client not getting IP in Bridged Mode
    Reason for change: When bridged mode is enabled on XB3 lan client is not getting a public IP
    Test Procedure: Enable bridged mode and verify that lan client is getting a public IP
    Swicth back to router mode and see that lan client is getting a 10.x.x.x IP
    Risks: l2sd0.100, l2sd0.101 should come up in all the cases
    Signed-off-by: Raghavendra Kammara <raghavendra_kammara@cable.comcast.com>
    
    Change-Id: I1fda721b2e010d83459317f336df876c20e8d215

--- source/service_multinet/Puma6_plat/puma6_plat_sw.c
+++ source/service_multinet/Puma6_plat/puma6_plat_sw.c
@@ -64,71 +64,79 @@ static int psm_get_record(const char *name, char *val, int size)
 }
 
 int configVlan_ESW(PSWFabHALArg args, int numArgs, BOOL up) {
-    int i, rc;
+    int i;
     PSwPortState portState;
-    
     char cmdBuff[128];
     char ifname[80];
-	char iot_enabled[20];
+    char temp_ifname[80];
+    char iot_enabled[20];
+    memset(ifname, 0, 80);
+    memset(temp_ifname, 0, 80);
+
+    if (100 == args[0].vidParams.vid) //brlan0 case
+    {   
+        for (i = 0; i < numArgs; ++i ) { 
     
-    for (i = 0; i < numArgs; ++i ) {
-        
-        portState = (PSwPortState) args[i].portID;
-        stringIDExtSw(portState, ifname, sizeof(ifname));
-        
-        //#Args: netid, netvid, members...
-        sprintf(cmdBuff, "%s %s %d %d \"%s%s\"", SERVICE_MULTINET_DIR "/handle_sw.sh", up ? "addVlan" : "delVlan", args[i].hints.network->inst, args[i].vidParams.vid, ifname, args[i].vidParams.tagging ? "-t" : "");
-        
-        system(cmdBuff);        
+            portState = (PSwPortState) args[i].portID;
+            stringIDExtSw(portState, ifname, sizeof(ifname));
+    
+            //#Args: netid, netvid, members...
+            sprintf(cmdBuff, "%s %s %d %d \"%s%s\"", SERVICE_MULTINET_DIR "/handle_sw.sh", up ? "addVlan" : "delVlan", args[i].hints.network->inst, args[i].vidParams.vid, ifname, args[i].vidParams.tagging ? "-t" : "");
+    
+            system(cmdBuff);
+        }
+    }   
+
+    else //brlan1 case
+    {   
+        for (i = 0; i < numArgs; ++i ) { 
+            portState = (PSwPortState) args[i].portID;
+            stringIDExtSw(portState, temp_ifname, sizeof(temp_ifname));
+            if (args[i].vidParams.tagging)
+                strcat(temp_ifname, "-t");
+
+            strcat(ifname, temp_ifname);
+            strcat(ifname, " ");
+        }
+        //Rag: netid and vlanid is same for all the args, so index zero is being used.
+        sprintf(cmdBuff, "%s %s %d %d \"%s\"", SERVICE_MULTINET_DIR "/handle_sw.sh", up ? "addVlan" : "delVlan", args[0].hints.network->inst, args[0].vidParams.vid, ifname);
+        MNET_DEBUG("configVlan_ESW, command is %s\n" COMMA cmdBuff)
+        system(cmdBuff);
     }
 
-	//brlan1 case check for iot
-	if (101 == args[0].vidParams.vid)
-	{
-		syscfg_init();
-        memset(iot_enabled, 0, 20);
-        rc = syscfg_get(NULL, "X_RDKCENTRAL-COM_ENABLEIOT", iot_enabled, sizeof(iot_enabled));
-
-        if((iot_enabled != NULL) || (rc != -1))
-        {
-	        printf("%s , IOT_LOG : iot_enabled is not NULL\n",__FUNCTION__);              
-            if(0 == strncmp(iot_enabled,"true",4))
-            {
-               // Add vlan for IOT 
-               memset(cmdBuff, 0, 128);
-               sprintf(cmdBuff, "%s %s %d %d \"%s\"", SERVICE_MULTINET_DIR "/handle_sw.sh", "addIotVlan", 0, 106,"-t");
-               printf("%s, IOT_LOG : Command for IOT %s\n",__FUNCTION__,cmdBuff);
-               system(cmdBuff);
-               printf("%s, IOT_LOG : Raise sysevent from multinet\n",__FUNCTION__);
-               sysevent_set(sysevent_fd_interactive, sysevent_token_interactive, "iot_status","bootup",0);
-            }
-            else
-            {
-               printf("%s, IOT_LOG : IOT is disabled\n",__FUNCTION__);
-            }
-        }
-        else
-        {
-           printf("%s, IOT_LOG : iot_enabled returned NULL\n",__FUNCTION__);
-        }
-	}    
 }
 
 int configVlan_WiFi(PSWFabHALArg args, int numArgs, BOOL up) {
     int i;
-    
-    char cmdBuff[128];
-    
-    for (i = 0; i < numArgs; ++i ) {
 
-        
-        //#Args: netid, netvid, members...
-        sprintf(cmdBuff, "%s %s %d %d \"%s%s\"", SERVICE_MULTINET_DIR "/handle_wifi.sh", up ? "addVlan" : "delVlan", args[i].hints.network->inst, args[i].vidParams.vid, (char*)args[i].portID, args[i].vidParams.tagging ? "-t" : "");
-        
-        system(cmdBuff);
-        
+    char cmdBuff[128];
+    char portID[80];
+    memset(portID, 0, 80);
+
+    if (100 == args[0].vidParams.vid)
+    {
+        for (i = 0; i < numArgs; ++i ) {
+            //#Args: netid, netvid, members...
+            sprintf(cmdBuff, "%s %s %d %d \"%s%s\"", SERVICE_MULTINET_DIR "/handle_wifi.sh", up ? "addVlan" : "delVlan", args[i].hints.network->inst, args[i].vidParams.vid, (char*)args[i].portID, args[i].vidParams.tagging ? "-t" : "");
+
+            system(cmdBuff);
+        }
     }
-    
+    else //blran1 case
+    {
+        for (i = 0; i < numArgs; ++i ) {
+            strcat(portID, (char*)args[i].portID);
+            if (args[i].vidParams.tagging)
+                strcat(portID, "-t");
+
+            strcat(portID, " ");
+        }
+
+        //Rag: netid and vlanid is same for all the args, so index zero is being used. 
+        sprintf(cmdBuff, "%s %s %d %d \"%s\"", SERVICE_MULTINET_DIR "/handle_wifi.sh", up ? "addVlan" : "delVlan", args[0].hints.network->inst, args[0].vidParams.vid, portID);
+        MNET_DEBUG("configVlan_WiFi, portId is:%s command is %s\n" COMMA portID COMMA cmdBuff)
+        system(cmdBuff);
+    }
 }
 
 int stringIDIntSw (void* portID, char* stringbuf, int bufSize) {
