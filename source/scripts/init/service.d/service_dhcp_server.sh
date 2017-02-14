0c57ba840e403883b8c9b16bcb1ada17b87bf31f
Raghavendra Kammara <raghavendra_kammara@cable.comcast.com>
2017-02-14 19:02:43 +0000

    RDKB-7044 : Develop Code - Boot Up information
    Reason for change: The boot information of Ethernet is logged to BootTime.log file.
    Test Procedure: Power on the box and let it boot completely
    Check whether /rdklogs/logs/BootTime.log has the timing information related to Ethernet.
    Risks: There shouldnt be any significant increase in the boot time.
    Signed-off-by: Raghavendra Kammara <raghavendra_kammara@cable.comcast.com>
    
    Change-Id: I9189f76d0ac998a03462fe9d663d7c3bf932c702

--- source/scripts/init/service.d/service_dhcp_server.sh
+++ source/scripts/init/service.d/service_dhcp_server.sh
@@ -438,6 +438,7 @@ dhcp_server_start ()
               	echo_t "RDKB_SYSTEM_BOOT_UP_LOG : Call gw_lan_refresh_from_dhcpscript:`uptime | cut -d "," -f1 | tr -d " \t\n\r"`"
               	gw_lan_refresh &
                 if [ ! -f "/tmp/gw_lan_refresh" ]; then
+                    print_uptime "boot_to_ETH_uptime"
                     echo "gw_lan_refresh is called for the first time notify SSID broadcast"
                     rpcclient $ATOM_ARPING_IP "touch /tmp/broadcast_ssids"
                     touch /tmp/gw_lan_refresh
