493bcbad62d5fdadf00196ad887954568ebded19
Raghavendra Kammara <raghavendra_kammara@cable.comcast.com>
2017-03-24 14:26:41 -0700

    RDKB-10698 : nslookup failing on ATOM
    Reason for change: dns_sync.sh is changed to handle the case where IPv4 DNS is not available
    Test Procedure: Check whether dns is synced properly and nslookup works
    Risks: None
    Signed-off-by: Raghavendra Kammara <raghavendra_kammara@cable.comcast.com>
    
    Change-Id: Ibfbf1cd8b139b303c23f80736cfbcb4f5e6f6e13

--- source/scripts/init/service.d/service_wan/dns_sync.sh
+++ source/scripts/init/service.d/service_wan/dns_sync.sh
@@ -1,24 +1,52 @@
 #!/bin/sh
 # This script will push the DNS from ARM to ATOM.
 #------------------------------------------------
-ATOM_RPC_IP=`cat /etc/device.properties | grep ATOM_ARPING_IP | cut -f 2 -d"="`
+TMP_RESOLV_FILE=/tmp/resolv.conf
+RESOLV_CONF=/etc/resolv.conf
+ATOM_USER_NAME=root
 retries=0
 
-# Synch the available DNS entries and wait until all entries are available.
-DNS_STR=`cat /etc/resolv.conf | grep nameserver | grep -v 127.0.0.1`
-rpcclient $ATOM_RPC_IP "echo \"$DNS_STR\" > /etc/resolv.conf"
+ATOM_INTERFACE_IP=`cat /etc/device.properties | grep ATOM_INTERFACE_IP | cut -f 2 -d"="`
+LAN_IP=`syscfg get lan_ipaddr`
 
 while :
 do
-        numOfEntries=`cat /etc/resolv.conf | grep nameserver | grep -v 127.0.0.1 | wc -l`
+        numOfEntries=`cat $RESOLV_CONF | grep nameserver | grep -v 127.0.0.1 | wc -l`
         if [ $numOfEntries -gt 2 -o $retries -eq 120 ]
         then
             break
         fi
 
-        sleep 1
+        sleep 5
         retries=$(( $retries + 1 ))
 done
 
-DNS_STR=`cat /etc/resolv.conf | grep nameserver | grep -v 127.0.0.1`
-rpcclient $ATOM_RPC_IP "echo \"$DNS_STR\" > /etc/resolv.conf"
+# Synch the available DNS entries and wait until all entries are available.
+DNS_STR=`cat $RESOLV_CONF | grep nameserver | grep -v 127.0.0.1`
+echo "$DNS_STR" > $TMP_RESOLV_FILE
+
+DNS_STR_V4=`cat $RESOLV_CONF | grep nameserver | grep -v 127.0.0.1 | grep "\." | cut -d" " -f2`
+if [ "$DNS_STR_V4" == "" ]
+then
+	echo "No IPv4 DNS entries available add default as lan IP"
+	echo "$LAN_IP" >> $TMP_RESOLV_FILE
+fi
+
+scp $TMP_RESOLV_FILE $ATOM_USER_NAME@$ATOM_INTERFACE_IP:$RESOLV_CONF > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	echo "scp is successful at first instance"
+else
+	retries=0
+	while :
+	do
+		scp $TMP_RESOLV_FILE $ATOM_USER_NAME@$ATOM_INTERFACE_IP:$RESOLV_CONF > /dev/null 2>&1
+		if [ $? -eq 0 -o  $retries -le 2 ]
+		then
+			echo "scp is successful at iteration:$retries"
+			break	
+		else
+			sleep 5
+			retries=$(( $retries + 1 ))
+		fi 
+	done	
+fi
