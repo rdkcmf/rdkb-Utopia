8d50745b0f9a53014203049dd18eb9ed4a08c7bd
smamidi <Srinivas_Mamidi@comcast.com>
2017-04-04 22:54:52 +0000

    ARRISXB6-3890:sometimes conntrack -D not working on XB6
    Reason for change: video pause is not working on blocked devices
    Test Procedure: Build and verify.
    Risks: Low.
    Signed-off-by: Srinivas_Mamidi@comcast.com
    
    Change-Id: Ia5eb91ddef4819fecc1c8884f4db992c6712ff53

--- source/firewall/firewall.c
+++ source/firewall/firewall.c
@@ -10301,6 +10301,11 @@ int CleanIPConntrack(char *physAddress)
    		 RmConntrackEntry(output);
     }
     pclose(fp);
+#if defined (INTEL_PUMA7)  
+  //ARRISXB6-3890 sometimes conntrack -D not working on XB6  
+    system("conntrack -F");
+#endif 
+
     return 0;
 }
 int IsFileExists(const char *fname)
