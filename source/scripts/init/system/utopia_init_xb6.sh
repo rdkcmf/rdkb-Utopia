33cb79a6bc76ae70d5fc4cab7930b98706da5dc0
gmarshall <Gordon_Marshall@cable.comcast.com>
2017-04-21 14:26:25 +0000

    ARRISXB6-4000: Log timestamp when device starts initialization
     along with the firmware name
    
    Reason for change:Log timestamp when device starts initialization-with firmware name
    Test Procedure: Check in /rdklogs/logs/Consolelog.txt.0 for the below print
    [Timestamp] [utopia][init] DEVICE_INIT:{Firmware_Name}
    Risks: None
    
    Change-Id: Ieb7654d56ac514c1143516561502e5fb95043f00
    Signed-off-by: gmarshall <Gordon_Marshall@cable.comcast.com>

--- source/scripts/init/system/utopia_init_xb6.sh
+++ source/scripts/init/system/utopia_init_xb6.sh
@@ -76,6 +76,8 @@ else
     debug_build=0
 fi
 
+firmware_name=`cat /version.txt | grep ^imagename: | cut -d ":" -f 2`
+echo "DEVICE_INIT:$firmware_name"
 
 echo "[utopia][init] Tweaking network parameters" > /dev/console
 echo "60" > /proc/sys/net/ipv4/netfilter/ip_conntrack_udp_timeout_stream
