3dd28e49017599bc9278e7c64641b53e89e1a47b
Priyadarshini Narayanan <Priyadarshini_Narayanan@cable.comcast.com>
2017-06-28 14:04:55 +0530

    RDKB-12301:Utopia contains snippet matches to libpcap (BSD)
    Reason for change:Utopia contains snippet matches to libpcap (BSD)
    Test Procedure:Added Lisence Copyright.No Test procedure Required.
    Risks: None
    Signed-off-by: Priyadarshini Narayanan <Priyadarshini_Narayanan@cable.comcast.com>
    
    Change-Id: Ia4b296cb21d62701fe78ac508147895ec5d98c27

--- source/utapi/lib/utapi_tr_wlan.c
+++ source/utapi/lib/utapi_tr_wlan.c
@@ -57,7 +57,29 @@
  * SYSTEMS, INC. EXPRESSLY DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED,
  * INCLUDING THE WARRANTIES OF MERCHANTIBILITY, FITNESS FOR A PARTICULAR
  * PURPOSE, TITLE, AND NONINFRINGEMENT.
- */
+ **********************************************************************/
+ 
+ /*Copyright (c) 2000 Torsten Landschoff <torsten@debian.org>
+ 		           Sebastian Krahmer  <krahmer@cs.uni-potsdam.de>
+License: BSD
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+  1. Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+  2. Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in
+     the documentation and/or other materials provided with the
+     distribution.
+  3. The names of the authors may not be used to endorse or promote
+     products derived from this software without specific prior
+     written permission.
+
+THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.*/
 
 #include <ctype.h>
 #include <stdio.h>
