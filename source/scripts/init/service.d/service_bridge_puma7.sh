031ead0869dcf0d8cfa356428b872b2052b6a639
Robert Lee <Robert_Lee2@comcast.com>
2017-08-08 20:52:54 +0000

    ARRISXB6-5082: Fix webUI access under bridge mode.
    Reason for change: Xconf Failing In Bridge Mode After changing the local
    network to 172.16.0.1 or 192.168.1.1
    Test Procedure: build and verify
    1) Verify router mode - access GW WebUI using 10.0.0.1 or 172.16.0.1
    2) Switch from router to bridge mode - access GW WebUI only 10.0.0.1.
    3) Boot from Bridge mode -  access GW WebUI only 10.0.0.1.
    Risks: MED
    Signed-off-by: Robert_Lee2@comcast.com
    
    Change-Id: I4c1f2aed1bbb6fe21fbcbe6870fdd094ef3813f7

--- source/scripts/init/service.d/service_bridge_puma7.sh
+++ source/scripts/init/service.d/service_bridge_puma7.sh
@@ -152,12 +152,12 @@ add_ebtable_rule()
     cmdiag_if=`syscfg get cmdiag_ifname`
     cmdiag_if_mac=`ip link show $cmdiag_if | awk '/link/ {print $2}'`
 
-    wan_if=`syscfg get wan_physical_ifname`
-    cmdiag_ip="192.168.100.1"
-    subnet_wan=`ip route show | awk '/'$wan_if'/ {print $1}'`
+    # wan_if=`syscfg get wan_physical_ifname`
+    # cmdiag_ip="192.168.100.1"
+    # subnet_wan=`ip route show | awk '/'$wan_if'/ {print $1}'`
 
-    ip route del $subnet_wan dev $wan_if
-    ip route add $subnet_wan dev $cmdiag_if #proto kernel scope link src $cmdiag_ip
+    #ip route del $subnet_wan dev $wan_if
+    #ip route add $subnet_wan dev $cmdiag_if #proto kernel scope link src $cmdiag_ip
 
     dst_ip="10.0.0.1" # RT-10-580 @ XB3 
     ip addr add $dst_ip/24 dev $cmdiag_if
@@ -176,12 +176,12 @@ del_ebtable_rule()
     cmdiag_if=`syscfg get cmdiag_ifname`
     cmdiag_if_mac=`ip link show $cmdiag_if | awk '/link/ {print $2}'`
 
-    wan_if=`syscfg get wan_physical_ifname`
-    wan_ip=`sysevent get ipv4_wan_ipaddr`
-    subnet_wan=`ip route show | grep $cmdiag_if | grep -v 192.168.100. | grep -v 10.0.0 | awk '/'$cmdiag_if'/ {print $1}'`
+    # wan_if=`syscfg get wan_physical_ifname`
+    # wan_ip=`sysevent get ipv4_wan_ipaddr`
+    # subnet_wan=`ip route show | grep $cmdiag_if | grep -v 192.168.100. | grep -v 10.0.0 | awk '/'$cmdiag_if'/ {print $1}'`
 
-    ip route del $subnet_wan dev $cmdiag_if
-    ip route add $subnet_wan dev $wan_if proto kernel scope link src $wan_ip
+    # ip route del $subnet_wan dev $cmdiag_if
+    # ip route add $subnet_wan dev $wan_if proto kernel scope link src $wan_ip
 
     dst_ip="10.0.0.1" # RT-10-580 @ XB3 PRD
     ip addr del $dst_ip/24 dev $cmdiag_if
