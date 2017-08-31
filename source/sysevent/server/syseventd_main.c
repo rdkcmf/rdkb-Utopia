2c45a01a3565c54d43c6353b549f439d507fd0cd
lshenbagaraj <Lakshminarayanan_Shenbagaraj@comcast.com>
2017-08-31 18:39:44 +0530

    RDKB-13770 : syseventd_fork and multiple sh processes are in Zombie state.
    Reason for change:
    We are missing to register below signal handler during syseventd creation. so that child process went to zombie state.
    signal(SIGCHLD, SIG_IGN);
    Test Procedure:
    1. Ensure [syseventd_fork_] this process should not list in process list
    2. Validate all box features because i made update in syseventd process.
    Risks: None
    Signed-off-by: Lakshminarayanan Shenbagaraj Lakshminarayanan_Shenbagaraj@comcast.com
    
    Change-Id: Ifcc33f31871dc76857cb482f0ce5f18c9cf9d010

--- source/sysevent/server/syseventd_main.c
+++ source/sysevent/server/syseventd_main.c
@@ -669,6 +669,30 @@ static int initialize_system(void)
         }
       }
    }
+#else
+	struct sigaction new_action, old_action;
+	
+	// we want to ignore SIGCHLD so that there wont be zombies
+	new_action.sa_handler = SIG_IGN;
+	new_action.sa_flags   = 0;
+	sigemptyset (&new_action.sa_mask);
+	if (-1 == sigaction (SIGCHLD, NULL, &old_action)) {
+	   SE_INC_LOG(ERROR,
+		  printf("Problem getting original signal handler for SIGCHLD. Reason (%d) %s\n", 
+				 errno, strerror(errno));
+	   )
+	   return(ERR_SIGNAL_DEFINE);
+	} else { 
+	   if (SIG_IGN != old_action.sa_handler) {
+		 if (-1 == sigaction (SIGCHLD, &new_action, NULL)) {
+			SE_INC_LOG(ERROR,
+			   printf("Problem setting signal handler for SIGCHLD. Reason (%d) %s\n", 
+					  errno, strerror(errno));
+			)
+			return(ERR_SIGNAL_DEFINE);
+		 }
+	   }
+	}
 #endif
 
    // save our pid in the pid file
