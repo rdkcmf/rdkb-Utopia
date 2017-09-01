5d8ab85ffe3c763bff318895c725adab21a1cce8
lshenbagaraj <Lakshminarayanan_Shenbagaraj@comcast.com>
2017-09-01 16:02:26 +0530

    RDKB-13759 : [Logging] Log the number of process active in ARM & ATOM.
    Reason for change:
    In order to track this in telemetry we need a logging in telemetry
    1. to print the total number of process in ARM and ATOM every 6 hours(eg: ps | wc -l)
    2. to print one log in the first SelfHeal check (15mins after bootup) and then 6hrs from there on
    3. to print a warning message if the total number of process go beyond a thresold at ARM and ATOM
    ARM process count threshold: 400
    ATOM process count threshold: 200
    Test Procedure:
    Ensure below logs should log in (/rdklogs/logs/SelfHeal.txt.0) file by below cases.
    1. Verify one log in the first SelfHeal check (15mins after bootup) and then 6hrs from there on
    170901-10:15:02.780000 TOTAL_ARM_Processes:203 <ARM PROCESS COUNT>
    170901-10:15:02.900000 TOTAL_ATOM_Processes:115 <ATOM PROCESS COUNT>
    2. Verify warning message if the total number of process go beyond a thresold at ARM for 400 and ATOM for 200
    170901-10:15:02.780000 ARM process count above threshold
    170901-10:15:02.900000 ATOM process count above threshold
    Risks: None
    Signed-off-by: Lakshminarayanan Shenbagaraj Lakshminarayanan_Shenbagaraj@comcast.com
    
    Change-Id: Ic2addcca799e2a2620b5278fbbed7397e0af22a8

--- source/scripts/init/service.d/service_crond.sh
+++ source/scripts/init/service.d/service_crond.sh
@@ -121,7 +121,7 @@ service_start ()
       #zqiu: monitor lan client traffic
       echo "* * * * *   /usr/ccsp/tad/rxtx_lan.sh" >> $CRONTAB_FILE
 
-      echo "1 */6 * * *   /usr/ccsp/tad/monitor_zombies.sh" >> $CRONTAB_FILE
+      echo "1 */6 * * *   /usr/ccsp/tad/monitor_process_info.sh" >> $CRONTAB_FILE
 #RDKB-9367, file handle monitor, needs to be run every 12 hours
       echo "1 */12 * * *   /usr/ccsp/tad/FileHandle_Monitor.sh" >> $CRONTAB_FILE
 
