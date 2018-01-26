845b6817095da264e8e6dc36fb23bb873ee7a77c
skv01 <Shibu_KV@cable.comcast.com>
2018-01-26 16:57:51 -0500

    RDKB-15072 : Enabling TOS mirroring rules for IPv6 traffic.
    Reason for change: Is a requirement for video client devices.
    This is similar to v4 rules added priviously as part of RDKB -4519
    Test Procedure: 1] Full regression testing on IPv4 and IPv6 related connectivity including LAN clients
    2] Ensure XI5 devices in DRI in GRAM or devices with SLAAC enabled mode.
       This needs to be verified on all flavors of XB combination
    Risks: Low
    Signed-off-by: skv01 <Shibu_KV@cable.comcast.com>
    
    Change-Id: I20827e2350561abd0ef0bb6e42f01cd92bd40ab6

--- source/firewall/firewall.c
+++ source/firewall/firewall.c
@@ -8875,7 +8875,12 @@ static int prepare_xconf_rules(FILE *mangle_fp) {
 #if ! defined (INTEL_PUMA7) && ! defined (_COSA_BCM_ARM_)
    fprintf(mangle_fp, "-A FORWARD -m state ! --state NEW -j DSCP  --set-dscp 0x0\n");
 #endif
-   fprintf(mangle_fp, "-A OUTPUT -o erouter0 -j DSCP --set-dscp-class af22\n");
+   /**
+    * RDKB-15072 - Explicitly specify proticol instead of common rule as workaround to overcome CMTS issue.
+    **/
+   fprintf(mangle_fp, "-A OUTPUT -o erouter0 -j DSCP --protocol udp --set-dscp-class af22\n");
+   fprintf(mangle_fp, "-A OUTPUT -o erouter0 -j DSCP --protocol tcp --set-dscp-class af22\n");
+
    fprintf(mangle_fp, "-A POSTROUTING -o erouter0 -p gre -j DSCP --set-dscp %d \n",greDscp);
    
    fprintf(mangle_fp, "-I PREROUTING -i erouter0 -m dscp --dscp-class af32 -j CONNMARK --set-mark 0xA\n");
@@ -10317,24 +10322,7 @@ static void do_ipv6_sn_filter(FILE* fp) {
         }
 	//RDKB-10248: IPv6 Entries issue in ip neigh show 2. Bring back TOS mirroring 
 
-#if 0
-        ////Commenting out all DSCP rules : RDKB-5254
-
-	//zqiu: XCONF >>  RDKB-4519
-		fprintf(fp, "-A FORWARD -m state --state NEW -j DSCP --set-dscp-class af22\n");
-		fprintf(fp, "-A FORWARD -m state ! --state NEW -j DSCP  --set-dscp 0x0\n");
-		fprintf(fp, "-A OUTPUT -o erouter0 -j DSCP --set-dscp-class af22\n");
-		fprintf(fp, "-A POSTROUTING -o erouter0 -p gre -j DSCP --set-dscp %d \n",greDscp);
-		fprintf(fp, "-I PREROUTING -i erouter0 -m dscp --dscp-class af32 -j CONNMARK --set-mark 0xA\n");
-		fprintf(fp, "-I PREROUTING -i erouter0 -m dscp --dscp-class cs1 -j CONNMARK --set-mark 0xB\n");
-		fprintf(fp, "-I PREROUTING -i erouter0 -m dscp --dscp-class cs5 -j CONNMARK --set-mark 0xC\n");
-		fprintf(fp, "-I PREROUTING -i erouter0 -m dscp --dscp-class af22 -j CONNMARK --set-mark 0xD\n");
-		fprintf(fp, "-A POSTROUTING -o erouter0 -m connmark --mark 0xA  -j DSCP --set-dscp-class af32\n");
-		fprintf(fp, "-A POSTROUTING -o erouter0 -m connmark --mark 0xB -j DSCP --set-dscp-class cs1\n");
-		fprintf(fp, "-A POSTROUTING -o erouter0 -m connmark --mark 0xC -j DSCP --set-dscp-class cs5\n");
-		fprintf(fp, "-A POSTROUTING -o erouter0 -m connmark --mark 0xD -j DSCP --set-dscp-class af22\n");	
-	//zqiu: XCONF <<
-#endif
+        prepare_xconf_rules(fp);
   
      FIREWALL_DEBUG("Exiting do_ipv6_sn_filter \n"); 
 }
