cdc7865e0ed1f825ba01c50e2b1171ef1d262c22
Lakshminarayanan Shenbagaraj <lakshminarayanan_shenbagaraj@comcast.com>
2018-06-04 17:44:56 +0530

    RDKB-15649 : Incorrect or missing copyright attributions in Utopia
    Reason for change: Addition/Correction of Copyright Attributes
    
    Test Procedure:
    -Builds should pass without any errors.
    -No regressions should be caused.
    
    Risks: None
    
    Change-Id: I87da0fe21d9040f038454b7293305cffb54a67af
    Signed-off-by: Athul Philip (athul.p@infosys.com)

--- source/scripts/init/service.d/service_multinet/handle_sw_puma7.sh
+++ source/scripts/init/service.d/service_multinet/handle_sw_puma7.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
-#############################################################################
-#  Copyright 2017 Intel Corporation
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
 #
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
+# Copyright 2018 RDK Management
+# Copyright 2017 Intel Corporation
 #
-#  http://www.apache.org/licenses/LICENSE-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-###############################################################################
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
 
 SWITCH_MAP_FILE="/var/tmp/map/mapping_table.txt"
 PORT_VLAN_START=200
