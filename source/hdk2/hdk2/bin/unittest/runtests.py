5e419e6e75bfee53fbb75aa1a5d7cf9226adf880
David Smith <dave.m.smith@accenture.com>
2018-06-18 16:37:38 +0100

    RDKBCMF-200: Correct shebang placement.
    Reason for change: Shebang placement incorrect.
    Test Procedure: Build only
    Risks: None
    Signed-off-by: Dave Smith <dave.m.smith@accenture.com>
    
    Change-Id: I4a54de7c8fa49475a9ff1d64bb3d8c1ae03dfb03

--- source/hdk2/hdk2/bin/unittest/runtests.py
+++ source/hdk2/hdk2/bin/unittest/runtests.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 ##########################################################################
 # If not stated otherwise in this file or this component's Licenses.txt
 # file the following copyright and licenses apply:
@@ -16,7 +17,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ##########################################################################
-#!/usr/bin/python
 
 ###########################################################################
 #
