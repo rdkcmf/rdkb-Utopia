0360c6372e466c0f926a5da62feedcf3bc3f2345
Thanh Nguyen <Thanh.Nguyen@technicolor.com>
2018-06-22 17:03:10 +0530

    RDKB3939-664 : Last reboot reason for watchdog
    Reason for change: Last reboot reason for watchdog failures
    Test Procedure: Create a scenario where fork bomb occurs, forcing the box to reboot
    reboot reason
    Risks: Low
    Signed-off-by: Aishwarya N <aishwarya.n@technicolor.com>
    
    Change-Id: Ie5ea29b4aa84fae053db55e836e9be2f9f220f20

--- source/scripts/init/system/utopia_init.sh
+++ source/scripts/init/system/utopia_init.sh
@@ -563,6 +563,12 @@ else
          elif [ "$MODEL_NUM" = "DPC3939" ] && [ "`cat /proc/P-UNIT/status|grep "Last reset origin"|awk '{ print $9 }'`" == "RESET_ORIGIN_ATOM" ] && [ ! -f "/nvram/RDKB3939-500_RebootNotByPwrOff" ]; then
          syscfg set X_RDKCENTRAL-COM_LastRebootReason "HW or Power-On Reset"
          syscfg set X_RDKCENTRAL-COM_LastRebootCounter "1"
+      elif [ -e "/proc/P-UNIT/status" ]; then
+         Punit_status=`grep -i "Last reset origin" /proc/P-UNIT/status | awk '{print $9}'`
+         if [ "$Punit_status" = "RESET_ORIGIN_ATOM_WATCHDOG" ] || [ "$Punit_status" = "RESET_ORIGIN_DOCSIS_WATCHDOG" ];then
+             syscfg set X_RDKCENTRAL-COM_LastRebootReason $Punit_status
+             syscfg set X_RDKCENTRAL-COM_LastRebootCounter "1"         
+         fi
 #endif
       else
          RESET_DURATION=`cat /proc/P-UNIT/status|grep "Last reset duration"|awk '{ print $7 }'`
