57bafe5fc0c1bd6a6919317c03ba145ec270da6a
gmarshall <Gordon_Marshall@cable.comcast.com>
2019-01-29 12:43:43 +0000

    RDKB-20496: 0061-ti_udhcp-initial-timeout-fix.patch
    Reason for change: Intel Proposed RDKB Bug Fix
    Test Procedure: See Jira Ticket Comments
    Risks: None
    
    Signed-off-by: gmarshall <Gordon_Marshall@cable.comcast.com>
    (cherry picked from commit b3f796f8a301c677e77a6761a0b9096566d16c9d)
    (cherry picked from commit eca129a7e949838ff6764a3a836c53b058251639)
    
    Change-Id: Ie47d147ac37a0c6e9ab4640d5f1561a4a12c88f4

--- source/service_wan/service_wan.c
+++ source/service_wan/service_wan.c
@@ -345,10 +345,16 @@ static int dhcp_start(struct serv_wan *sw)
     }
    else
    {
-
+#if defined (INTEL_PUMA7)
+    //Intel Proposed RDKB Generic Bug Fix from XB6 SDK
+    err = vsystem("ti_udhcpc -plugin /lib/libert_dhcpv4_plugin.so -i %s "
+                 "-H DocsisGateway -p %s -B -b 4",
+                 sw->ifname, DHCPC_PID_FILE);
+#else
     err = vsystem("ti_udhcpc -plugin /lib/libert_dhcpv4_plugin.so -i %s "
                  "-H DocsisGateway -p %s -B -b 1",
-                 sw->ifname, DHCPC_PID_FILE);
+                 sw->ifname, DHCPC_PID_FILE);   
+#endif
    }
    }
 #else
