893b14a0a3ef0e20fb131c0fe797875561c3f90a
Sachin Kitnawat <Sachin.Kitnawat@cognizant.com>
2019-03-07 05:04:24 +0000

    RDKCOM-887: BD-675 Utopia unknown cisco copyrights
    Reason for change: Removing unwanted cisco copyright
    Test Procedure: Build using Jenkins
    Risks: None
    Signed-off-by: Sachin Kitnawat <Sachin.Kitnawat@cognizant.com>
    
    Change-Id: I6527e57c04bb704f485b2d9166b567d30213d024

--- source/scripts/init/system/utopia_init.sh
+++ source/scripts/init/system/utopia_init.sh
@@ -17,18 +17,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ##########################################################################
-
-#------------------------------------------------------------------
-# Copyright (c) 2008 by Cisco Systems, Inc. All Rights Reserved.
-#
-# This work is subject to U.S. and international copyright laws and
-# treaties. No part of this work may be used, practiced, performed,
-# copied, distributed, revised, modified, translated, abridged, condensed,
-# expanded, collected, compiled, linked, recast, transformed or adapted
-# without the prior written consent of Cisco Systems, Inc. Any use or
-# exploitation of this work without authorization could subject the
-# perpetrator to criminal and civil liability.
-#------------------------------------------------------------------
+#######################################################################
+#   Copyright [2014] [Cisco Systems, Inc.]
+# 
+#   Licensed under the Apache License, Version 2.0 (the \"License\");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+# 
+#       http://www.apache.org/licenses/LICENSE-2.0
+# 
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an \"AS IS\" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#######################################################################
 
 #------------------------------------------------------------------
 #   This file contains the code to initialize the board
