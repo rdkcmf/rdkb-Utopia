4fea5be4af81ed248155510ce7c389356f569bfe
Abhishek Mishra <abhmishra.contractor@libertyglobal.com>
2019-08-21 20:10:37 +0200

    RDKBDEV-50 - Adding DSLite Functionality and related TR-181 DataModel Parameters
    Reason For Change:
        Implemenation of DSLite Functionality and related TR-181 DataModel Parameters
            Device.DSLite.Enable
            Device.DSLite.InterfaceSettingNumberOfEntries
            Device.DSLite.InterfaceSetting.{i}.Enable
            Device.DSLite.InterfaceSetting.{i}.Status
            Device.DSLite.InterfaceSetting.{i}.Alias
            Device.DSLite.InterfaceSetting.{i}.EndpointAssignmentPrecedence
            Device.DSLite.InterfaceSetting.{i}.EndpointAddressTypePrecedence
            Device.DSLite.InterfaceSetting.{i}.EndpointAddressInUse
            Device.DSLite.InterfaceSetting.{i}.EndpointName
            Device.DSLite.InterfaceSetting.{i}.EndpointAddress
            Device.DSLite.InterfaceSetting.{i}.Origin
            Device.DSLite.InterfaceSetting.{i}.TunnelInterface
            Device.DSLite.InterfaceSetting.{i}.TunneledInterface
            Device.DSLite.InterfaceSetting.{i}.X_RDKCENTRAL-COM_MssClampingEnable
            Device.DSLite.InterfaceSetting.{i}.X_RDKCENTRAL-COM_Tcpmss
            Device.DSLite.InterfaceSetting.{i}.X_RDKCENTRAL-COM_IPv6FragEnable
    
        Added the dependent DSLite changes for Utopia and added compilation flag "DSLITE_FEATURE_SUPPORT" for DSLite feature compilation with distro feature 'dslite'.
        Added conditional compilation --enable-dslite_feature_support in configure.ac for service_dslite compilation.
    Test Procedure:
        Detailed Test plan and Report has been attached into the JIRA ticket.
    
    Risks: None
    
    Signed-off-by: Abhishek Mishra<abhmishra.contractor@libertyglobal.com>
    
    Conflicts:
    	source/include/utctx/utctx_api.h
    	source/scripts/init/defaults/system_defaults_arm
    	source/utapi/lib/Makefile.am
    	source/utctx/lib/utctx.c
    	source/utctx/lib/utctx_api.h
    Change-Id: I34458832e820e0c55a1aa0961f8339ef07c7ebc2

---
+++ source/utapi/lib/utapi_dslite.h
@@ -0,0 +1,59 @@
+/*#####################################################################
+# Copyright 2017-2019 ARRIS Enterprises, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+######################################################################*/
+
+#ifndef __UTAPI_TR_DSLITE_H__
+#define __UTAPI_TR_DSLITE_H__
+
+#define STR_SZ 257
+#define MAX_NUM_INSTANCES 255
+
+typedef struct
+DsLiteCfg
+{
+    unsigned long  InstanceNumber;
+    int            active;
+    int            status;
+    char           alias[64+1];
+    int            mode;
+    int            addr_type;
+    char           addr_inuse[256+1];
+    char           addr_fqdn[256+1];
+    char           addr_ipv6[256+1];
+    int            origin;
+    char           tunnel_interface[256+1];
+    char           tunneled_interface[256+1];
+    int            mss_clamping_enable;
+    unsigned long  tcpmss;
+    int            ipv6_frag_enable;
+}DsLiteCfg_t;
+
+int Utopia_GetDsliteEnable(UtopiaContext *ctx, boolean_t *bEnabled);
+int Utopia_SetDsliteEnable(UtopiaContext *ctx, boolean_t bEnabled);
+int Utopia_GetNumOfDsliteEntries(UtopiaContext *ctx,unsigned long *cnt);
+int Utopia_SetNumOfDsliteEntries(UtopiaContext *ctx,unsigned long cnt);
+int Utopia_GetDsliteCfg(UtopiaContext *ctx,DsLiteCfg_t *pDsliteCfg);
+int Utopia_SetDsliteCfg(UtopiaContext *ctx,DsLiteCfg_t *pDsliteCfg);
+int Utopia_AddDsliteEntry(UtopiaContext *ctx, DsLiteCfg_t *pDsliteCfg);
+int Utopia_DelDsliteEntry(UtopiaContext *ctx, unsigned long ulInstanceNumber);
+
+int Utopia_GetDsliteEntry(UtopiaContext *ctx,unsigned long ulIndex, void *pDsliteEntry);
+int Utopia_SetDsliteInsNum(UtopiaContext *ctx, unsigned long ulIndex, unsigned long ulInstanceNumber);
+
+/* Utility functions */
+int Utopia_GetDsliteByIndex(UtopiaContext *ctx, unsigned long ulIndex, DsLiteCfg_t *pDsLiteCfg_t);
+int Utopia_SetDsliteByIndex(UtopiaContext *ctx, unsigned long ulIndex, DsLiteCfg_t *pDsLiteCfg_t);
+
+#endif // __UTAPI_TR_DSLITE_H__
