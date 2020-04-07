8d61e230507da50ce0ddcedd03e0f9c9c863fe7a
Anoop Nair Chelakkode <anoopnair_chelakkode@cable.comcast.com>
2020-04-06 22:07:56 -0700

    RDKB-27966: NAT binding idle timeout
    Reason for change: As per  RFC 5382, NAT idle-timeout should not be less tha 2 hours 4 minutes.
    
    Test Procedure:
    1) Check if internet connectivity is good
    2) No CPU/memory increase with many connections opened
    3) No regression on advanced security
    
    Risks: None
    Signed-off-by: anoopnair_chelakkode@cable.comcast.com
    (cherry picked from commit 6ab1cbafde8d5f6ead8ade22b0bd36b60a3236af)
    Change-Id: Ib5213d735c6e1eee6c2aa57dcaf154ee7a0a21a6

--- source/scripts/init/system/utopia_init_arm.sh
+++ source/scripts/init/system/utopia_init_arm.sh
@@ -79,7 +79,7 @@ if [ $KERNEL_VERSION -lt 4 ] ; then
 	echo "10" > /proc/sys/net/ipv4/netfilter/ip_conntrack_tcp_timeout_time_wait
 	echo "10" > /proc/sys/net/ipv4/netfilter/ip_conntrack_tcp_timeout_close
 	echo "20" > /proc/sys/net/ipv4/netfilter/ip_conntrack_tcp_timeout_close_wait
-	echo "1800" > /proc/sys/net/ipv4/netfilter/ip_conntrack_tcp_timeout_established
+	echo "7440" > /proc/sys/net/ipv4/netfilter/ip_conntrack_tcp_timeout_established
 	echo "8192" > /proc/sys/net/ipv4/netfilter/ip_conntrack_max
 else
 	echo "60" > /proc/sys/net/netfilter/nf_conntrack_udp_timeout_stream
@@ -88,7 +88,7 @@ else
 	echo "10" > /proc/sys/net/netfilter/nf_conntrack_tcp_timeout_time_wait
 	echo "10" > /proc/sys/net/netfilter/nf_conntrack_tcp_timeout_close
 	echo "20" > /proc/sys/net/netfilter/nf_conntrack_tcp_timeout_close_wait
-	echo "1800" > /proc/sys/net/netfilter/nf_conntrack_tcp_timeout_established
+	echo "7440" > /proc/sys/net/netfilter/nf_conntrack_tcp_timeout_established
 	echo "8192" > /proc/sys/net/netfilter/nf_conntrack_max
 fi
 
