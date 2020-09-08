3b3e28fe97a40bc85790baf94ff25fc6b91a7aad
knair410 <Krishnan_Nair@comcast.com>
2020-09-08 10:36:38 +0530

    RDKB-31312 : Aker files are not deleted from nvram after Factory reset.
    Reason for change: Remove pcs.bin and pcs.bin.mdf at Factory reset.
    Test Procedure: Verify after factory rest Aker schedule files are  properly deleted from nvram.
    Risks: None
    Signed-off-by: krishnan_nair@cable.comcast.com
    (cherry picked from commit b949a2277703fc1553a65ca222a43fac8adb5d64)
    Change-Id: Ibb5d76d24012dde89f357c717340a40d53ce8305

--- source/scripts/init/system/utopia_init_arm.sh
+++ source/scripts/init/system/utopia_init_arm.sh
@@ -350,6 +350,11 @@ fi
        # Remove webconfig_db.bin on factory reset on all RDKB platforms
         rm -f /nvram/webconfig_db.bin        
    fi
+   if [ -f /etc/AKER_ENABLE ]; then
+      # Remove on factory reset, Aker schedule pcs.bin and pcs.bin.md5 on all RDKB platforms 
+      rm -f /nvram/pcs.bin
+      rm -f /nvram/pcs.bin.md5
+   fi
      touch /nvram/.apply_partner_defaults   
    #>>zqiu
    create_wifi_default
