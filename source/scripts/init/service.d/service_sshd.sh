36b4fe35f22a264915f70ae0d9010fb9c543d39a
apalan608 <arunkumar_palanichamy@comcast.com>
2022-02-11 17:33:08 +0530

    RDKB-39480: CMXB7 wan manager integration.
    Reason for change:
    1. wan ipv4/ipv6 clients are started
      from selfhealagressive without checking wan status.
    
    Test Procedure:
    1. Test ipv6/ipv6 connectivity wan and lan.
    2. Test Brigde mode and docsis mode, Ethwan mode.
    3. Mac address leak in cpe table.
    
    Risks: None
    Signed-off-by: arunkumar_palanichamy@comcast.com
    (cherry picked from commit f04206b70c5d9afd743faf7d7bc822c9391e0f8a)
    Change-Id: Id6965794941ef5de68f6f8ac313685ca58301449

--- source/scripts/init/service.d/service_sshd.sh
+++ source/scripts/init/service.d/service_sshd.sh
@@ -87,7 +87,7 @@ get_listen_params() {
         CM_IP4=`ip -4 addr show dev $CMINTERFACE scope global | awk '/inet/{print $2}' | cut -d '/' -f1`
     fi
     if ([ "$BOX_TYPE" = "XB6" -a "$MANUFACTURE" = "Arris" ]); then
-        CM_IP6=`ip -6 addr show dev $CMINTERFACE scope global | awk '/inet/{print $2}' | cut -d '/' -f1 | head -n1`
+        CM_IP6=`ip -6 addr show dev $CMINTERFACE scope global | awk '/inet/{print $2}' | cut -d '/' -f1 | head -1`
     fi
     if [ "$CM_IP4" != "" ] ; then
         LISTEN_PARAMS="-p [${CM_IP4}]:22"
