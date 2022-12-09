28e7b70d95a092c07860a894458a023c48adf67f
Sivapalan Ayyappan <sivapalan.ayyappan@sky.uk>
2022-12-09 11:12:27 +0000

    SKYH4-6551 : VAR_TMPFS_FULL RebootReason
    Reason for change:
    /var/log/user.log and /var/tmp/zebra.log
    using more space under /var/.
    Handled /var/tmp/zebra.log
    
    Test Procedure:
    Make sure /var/tmp/zebra is not using more than 1 MB
    and files inside /var/log is not using more than 3MB
    
    Risks: Low
    
    Priority: P1
    
    Signed-off-by: Sivapalan Ayyappan <sivapalan.ayyappan@sky.uk>
    (cherry picked from commit 7ab1bb9dbb7fb6be0b162c59081adfd2dfd72b24)
    Change-Id: I2723876cd1743cd28cd2b12860279b88bda93547

--- source/scripts/init/service.d/logrotate.sh
+++ source/scripts/init/service.d/logrotate.sh
@@ -5,9 +5,14 @@ source /etc/device.properties
 if [ "$BOX_TYPE" == "HUB4" ]; then
 source /etc/utopia/service.d/log_capture_path.sh
 VARLOG_DIR_THRESHOLD=3000
+VAR_TMP_FILE_THRESHOLD=1000
+tmp_file_size=0
 else
 VARLOG_DIR_THRESHOLD=5000
 fi
+
+#Save the filename which is emptied last with time
+save_file=/tmp/.logrotation.txt
 
 dir=`du /var/log/ | awk -v sum=0 '{print sum+=$1}' | tail -1`
 
@@ -58,6 +63,7 @@ if [ $ksize -gt $VARLOG_DIR_THRESHOLD ]; then
           fi
 
           cat /dev/null > /var/log/"$i"
+          echo "[logrotate.sh] Emptied /var/log/$i at $(date)" > $save_file
        fi  
     done
 fi
@@ -70,11 +76,25 @@ if [ $dir -gt $VARLOG_DIR_THRESHOLD ]; then
             if [ "$file" == "dibbler" ];  then
                     cat /dev/null > /var/log/dibbler/dibbler-client.log
                     cat /dev/null > /var/log/dibbler/dibbler-server.log
+                    echo "[logrotate.sh] Emptied /var/log/$file at $(date)" > $save_file
             else
                     cat /dev/null > /var/log/"$file"
+                    echo "[logrotate.sh] Emptied /var/log/$file at $(date)" > $save_file
             fi
         fi
      done
+fi
+
+# If any file reaches 1MB inside /var/tmp, empty the file.
+if [ "$BOX_TYPE" == "HUB4" ]; then
+    var_tmp_files=`ls /var/tmp/`
+    for tmp_file in $var_tmp_files; do
+        tmp_file_size=`du /var/tmp/$tmp_file | awk -v sum=0 '{print sum+=$1}' | tail -1`
+        if [ $tmp_file_size -gt $VAR_TMP_FILE_THRESHOLD ]; then
+            cat /dev/null > /var/tmp/$tmp_file
+            echo "[logrotate.sh] Emptied /var/tmp/$tmp_file at $(date)" > $save_file
+        fi
+    done
 fi
 
 exit 0
